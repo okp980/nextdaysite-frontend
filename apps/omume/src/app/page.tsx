908818ebd5944af24df6cbc81e0ec454ba2341b8
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-01 12:31:42 +0300

    feat: project setup

---
+++ apps/omume/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@nextdaysite/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Button
+        appName="omume"
+        className="bg-white text-red-800 px-5 py-3 rounded-sm"
+      >
+        Welcome
+      </Button>
+    </div>
+  );
+}
