880a8d9cecaf9c303627528c3e6241842102424f
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-02 20:38:59 +0300

    feat(omume): added page structure

---
+++ apps/omume/src/app/(protected)/(dashboard)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { PropsWithChildren } from "react"
+
+type Props = {}
+
+const DashboardLayout = ({ children }: PropsWithChildren) => {
+  return <div>{children}</div>
+}
+
+export default DashboardLayout
