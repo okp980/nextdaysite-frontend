cd36412663f2fcbd26f75fb072af6c8e041a27bb
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-07 11:54:10 +0300

    chore: onboarding pages structure

--- apps/omume/src/app/(protected)/onboarding/layout.tsx
+++ apps/omume/src/app/(protected)/onboarding/layout.tsx
@@ -1,9 +1,7 @@
-import React from "react";
-
 export default function OnboardingLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
-  return <div>{children}</div>;
+  return <div>{children}</div>
 }
