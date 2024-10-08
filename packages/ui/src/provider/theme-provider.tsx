46c14c630976d20b7ada49a28c13af3f2c51fdeb
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-08 17:17:30 +0300

    fix: changed theme

--- packages/ui/src/provider/theme-provider.tsx
+++ packages/ui/src/provider/theme-provider.tsx
@@ -1,9 +1,9 @@
-"use client";
-import { ThemeProvider, useTheme } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
+"use client"
+import { ThemeProvider, useTheme } from "next-themes"
+import { ThemeProviderProps } from "next-themes/dist/types"
 
 export default function (props: ThemeProviderProps) {
-  return <ThemeProvider attribute="class" defaultTheme="system" {...props} />;
+  return <ThemeProvider attribute="class" defaultTheme="light" {...props} />
 }
 
-export { useTheme };
+export { useTheme }
