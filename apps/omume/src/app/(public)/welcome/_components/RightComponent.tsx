c8570af025785cacc96ca04ec0d0ab359de8e5a4
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-14 20:59:25 +0300

    fix: sign in and sign in links on welcome page

--- apps/omume/src/app/(public)/welcome/_components/RightComponent.tsx
+++ apps/omume/src/app/(public)/welcome/_components/RightComponent.tsx
@@ -1,24 +1,26 @@
-"use client";
-import Button from "@nextdaysite/ui/button";
-import React from "react";
-import { useRouter } from "next/navigation";
+"use client"
+import Button from "@nextdaysite/ui/button"
+import React from "react"
+import { useRouter } from "next/navigation"
+import Link from "next/link"
 
-type Props = {};
+type Props = {}
 
 export default function RightComponent({}: Props) {
-  const router = useRouter();
+  const router = useRouter()
   return (
     <div className="flex gap-2">
-      <Button variant="light" className="font-normal w-20">
+      <Button
+        variant="light"
+        className="font-normal w-20"
+        as={Link}
+        href="/signin"
+      >
         Sign in
       </Button>
-      <Button
-        color="primary"
-        onClick={() => router.push("/signup")}
-        className="w-32"
-      >
+      <Button color="primary" className="w-32" as={Link} href="/signup">
         Sign Up
       </Button>
     </div>
-  );
+  )
 }
