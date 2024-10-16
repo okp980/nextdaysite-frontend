efc33e1e844d275ab712af4f576b3bb7c1931f09
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-16 16:21:13 +0300

    fix: auth nav fix

--- apps/omume/src/app/(auth)/layout.tsx
+++ apps/omume/src/app/(auth)/layout.tsx
@@ -1,13 +1,15 @@
-import React, { PropsWithChildren } from "react";
-import Nav from "@nextdaysite/ui/nav-bar";
+import React, { PropsWithChildren } from "react"
+import Nav from "@nextdaysite/ui/nav-bar"
 
-type Props = {};
+type Props = {}
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div>
-      <Nav />
+      <div className="fixed left-0 w-full z-10">
+        <Nav />
+      </div>
       {children}
     </div>
-  );
+  )
 }
