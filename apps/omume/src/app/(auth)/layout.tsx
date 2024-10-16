f16b97b1816847941764fe7557daa40a5fbc4ef0
Okpunor Emmanuel <okpunorrex@gmail.com>
2024-10-16 16:23:21 +0300

    fix: fix the auth layout

--- apps/omume/src/app/(auth)/layout.tsx
+++ apps/omume/src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
-import React, { PropsWithChildren } from "react"
-import Nav from "@nextdaysite/ui/nav-bar"
+import React, { PropsWithChildren } from "react";
+import Nav from "@nextdaysite/ui/nav-bar";
 
-type Props = {}
+type Props = {};
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -11,5 +11,5 @@ export default function Layout({ children }: PropsWithChildren) {
       </div>
       {children}
     </div>
-  )
+  );
 }
